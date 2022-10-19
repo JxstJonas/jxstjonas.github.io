85aea93fab55d087f67e1f87b2ef554f17796103
Jonas <58705742+JxstJonas@users.noreply.github.com>
2022-10-19 23:14:01 +0200

    add: Particle to Secret

---
+++ components/Particle.tsx
@@ -0,0 +1,136 @@
+import { useCallback } from "react";
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+
+export const Dennis = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+
+    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container: Container | undefined) => {
+    await console.log(container);
+  }, []);
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        "particles": {
+          "number": {
+            "value": 9,
+            "density": {
+              "enable": true,
+              "value_area": 800
+            }
+          },
+          "color": {
+            "value": "#ffffff"
+          },
+          "shape": {
+            "type": "image",
+            "stroke": {
+              "width": 0,
+              "color": "#000000"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "src": "https://icons.veryicon.com/png/o/food--drinks/fruit-icon-1/avocado-23.png",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 1,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 15.782952832645451,
+            "random": true,
+            "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": false,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 1.5782952832645452,
+            "direction": "bottom",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": false,
+              "mode": "repulse"
+            },
+            "onclick": {
+              "enable": false,
+              "mode": "push"
+            },
+            "resize": true
+          },
+          "modes": {
+            "grab": {
+              "distance": 400,
+              "line_linked": {
+                "opacity": 1
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true
+      }}
+    />
+  );
+};
---
+++ pages/index.tsx
@@ -0,0 +1,69 @@
+import styles from '../styles/Home.module.css'
+import Image from 'next/image';
+import Link from 'next/link';
+
+//Image
+import GitIcon from '../public/img/github.png'
+import Avocado from '../public/img/avocado.png'
+
+import * as React from 'react';
+import { tsParticles } from "tsparticles-engine";
+import Script from 'next/script';
+import { Particles } from '../components/Particle';
+
+export default function Home() {
+    let [click,setclick] = React.useState(0);
+    const [display, setDisplay] = React.useState(false);
+    const [avocado, setAvocado] = React.useState(false);
+    const animation = async () => {
+        setAvocado(true);
+    }
+
+    React.useEffect(() => {
+
+        function handleClick() {
+            setclick(click + 1);
+            
+            console.log(click);
+            if(click == 9) {
+                setDisplay(true);
+            }
+        }
+
+        window.addEventListener("click", handleClick);
+        return () => {
+            window.removeEventListener("click", handleClick);
+        };
+    }, [click]);
+
+
+    return (
+        <div className={styles.noSelect}>
+            <div style={{zIndex: 1}} className={styles.middle}>
+                <div className={styles.grid} >
+                    <div className={styles.flexSection}>
+                        <h1 className={styles.title}>Hello, I&apos;m Jonas</h1>
+                    </div>
+                    <div>
+                        <p className={styles.textBox}>
+                            I have been programming with the Java programming language for over 4 years. 
+                            I also work with databases such as Redis, MongoDB and MySQL. 
+                            And recently I started with web development.
+                        </p>
+                    </div>
+                    <div className={styles.flex}>
+                        <div className={styles.icon}>                        
+                            <a href='https://github.com/JxstJonas' target='_blank' rel='noreferrer'>
+                                <Image src={GitIcon} className={styles.gitIcon} alt='github'/>
+                            </a>
+                        </div>
+                        <div className={styles.secret} onClick={() => animation()} style={{display: display ? 'block' : 'none'}}>
+                            <Image src={Avocado} alt=''/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {avocado&&<Particles/>}
+        </div>
+    );
+}
